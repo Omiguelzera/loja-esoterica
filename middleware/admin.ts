1e4f6ae26eb3e3e4d5601434f80c0a1afe76a099
Omiguelzera <miguelgsantos98@gmail.com>
2025-10-02 18:11:19 -0300

    fix: missing import

--- middleware/admin.ts
+++ middleware/admin.ts
@@ -1,3 +1,6 @@
+import { useAuth } from "@/composables/useAuth.js"
+import { defineNuxtRouteMiddleware, navigateTo } from "nuxt/app"
+
 export default defineNuxtRouteMiddleware((to, from) => {
   // Este middleware roda apenas no cliente
   if (process.server) return
